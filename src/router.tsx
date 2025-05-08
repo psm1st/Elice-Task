c26d2093354f32908e4aee825f442751b24f81bc
psm1st <psm1st@sookmyung.ac.kr>
2025-05-09 01:47:38 +0900

    fix : error 수정

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainPage from "./pages/mainPage";
+
 const router = createBrowserRouter([
-    
+  {
+    path: "/",
+    element: <MainPage />,
+  },
 ]);
+
 export default router;
