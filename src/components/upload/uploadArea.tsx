8ae20364158d9a7050a1bb63c4f3b2c47a32e8a9
psm1st <psm1st@sookmyung.ac.kr>
2025-05-09 01:46:51 +0900

    feat : upload zone 개발

---
+++ src/components/upload/uploadArea.tsx
@@ -0,0 +1,41 @@
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import styled from "styled-components";
+
+
+type Props = {
+  onUpload: (zip: File) => void;
+};
+
+const UploadArea: React.FC<Props> = ({ onUpload }) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const zipFile = acceptedFiles.find((file) => file.name.endsWith(".zip"));
+    if (zipFile) onUpload(zipFile);
+  }, [onUpload]);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    multiple: false,
+    accept: {
+      "application/zip": [".zip"],
+    },
+  });
+
+  return (
+    <UploadBox {...getRootProps()}>
+      <input {...getInputProps()} />
+      ZIP 파일을 업로드하거나 이곳에 드래그 앤 드롭하세요
+    </UploadBox>
+  );
+};
+
+export default UploadArea;
+
+const UploadBox = styled.div`
+  border: 2px dashed #aaa;
+  border-radius: 8px;
+  padding: 40px;
+  text-align: center;
+  cursor: pointer;
+  background-color: #f8f8f8;
+`;
