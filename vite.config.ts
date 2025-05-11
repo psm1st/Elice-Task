d88e2ec9fa0628c62e0e1e0c55fbdf0110c4866a
psm1st <psm1st@sookmyung.ac.kr>
2025-05-11 13:37:23 +0900

    fix : vercel 배포 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    monacoEditorPlugin({
-      languageWorkers: ['editorWorkerService', 'typescript'],
-    }),
-  ],
+  plugins: [react()],
 });
